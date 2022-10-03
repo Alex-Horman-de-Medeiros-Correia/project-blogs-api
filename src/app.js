becc27bf8ad6c6250a55e3c04e0e56b303af8cd4
Alex-Horman-de-Medeiros-Correia <alexhorman1990@gmail.com>
2022-10-03 14:51:47 -0300

    modificando os controllers e suas chamadas

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const loginController = require('./controllers/loginController');
-const userController = require('./controllers/userController');
-const categoryController = require('./controllers/categoryController');
-const postController = require('./controllers/postController');
+const controlL = require('./controllers/controlL');
+const controlU = require('./controllers/controlU');
+const categoriaC = require('./controllers/categoriaC');
+const controlP = require('./controllers/controlP');
 
 // ...
 
 const app = express();
 
 app.use(express.json());
-app.use('/login/', loginController);
-app.use('/user/', userController);
-app.use('/categories/', categoryController);
-app.use('/post/', postController);
+app.use('/login/', controlL);
+app.use('/user/', controlU);
+app.use('/categories/', categoriaC);
+app.use('/post/', controlP);
 
 // ...
 
